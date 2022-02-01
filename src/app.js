0edbb45aa5ba98f1b5a6f31f714fc1581d01ad61
Romaine Allen <romallen1@gmail.com>
2022-02-01 17:32:42 +0900

    Import chart component into app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
+import HChart from "./component/chart"
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>JAMAICA STOCK EXCHANGE CHARTS</h1>
+      <HChart/>
     </div>
   );
 }
