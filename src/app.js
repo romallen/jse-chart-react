975bbbc32bd7dc033a7979145b77554363dd4580
Romaine Allen <romallen1@gmail.com>
2022-02-05 15:12:39 +0900

    Add navbar

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./style.css";
 import HChart from "./component/chart"
 import Footer from "./component/footer";
-
+import Navbar from "./component/navbar";
 export default function App() {
   return (
     <div className="chart-container">
+      <Navbar/>
       <h1 className="heading">JAMAICA STOCK EXCHANGE CHARTS</h1>
       <HChart/>
       <Footer/>
---
+++ src/component/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Navbar() {
+  return (
+    <div className="navbar-container">
+        <a className="nav-anchor" href = "https://romallen.com">
+        <p className="nav-text">Portfolio.</p>
+        </a>
+    </div>
+  );
+}
