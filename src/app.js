aa40ff337786dbcd893caaf06d89da3f5afc1902
Romaine Allen <romallen1@gmail.com>
2022-03-11 14:03:41 +0900

    Add grommit to project

--- src/app.js
+++ src/app.js
@@ -3,13 +3,27 @@ import "./style.css";
 import HChart from "./component/chart"
 import Footer from "./component/footer";
 import Navbar from "./component/navbar";
+import { Grommet } from 'grommet';
 export default function App() {
+  const theme = {
+    global: {
+      font: {
+        family: 'Roboto',
+        size: '14px',
+        height: '20px',
+      },
+    },
+  };
+  
   return (
-    <div className="chart-container">
+    <Grommet theme={theme}>
       <Navbar/>
       <h1 className="heading">JAMAICA STOCK EXCHANGE CHARTS</h1>
       <HChart/>
       <Footer/>
-    </div>
+      </Grommet>
+    // <div className="chart-container">
+
+    // </div>
   );
 }
